bb2c1323ca43039a539d55272d847da2688b4f61
israel0 <seyeakinsola@gmail.com>
2025-12-02 14:51:38 +0100

    chore: listing and vote controllers

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\VoteRequest;
 use App\Services\VoteService;
 use App\DTO\VoteDTO;
-use Illuminate\Http\JsonResponse;
 
 class VoteController extends Controller
 {
@@ -14,15 +13,5 @@ class VoteController extends Controller
         protected VoteService $voteService
     ) {}
 
-    public function toggle(VoteRequest $request): JsonResponse
-    {
-        $dto = VoteDTO::fromRequest($request->validated());
-        $vote = $this->voteService->toggleVote($dto);
 
-        return response()->json([
-            'success' => true,
-            'vote' => $vote,
-            'message' => 'Vote updated successfully'
-        ]);
-    }
 }
